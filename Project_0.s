19c6f99bc454fa44ff8a6fbe2972ac59e74bc28f
Tahmauri01 <113442519+Tahmauri01@users.noreply.github.com>
2025-03-10 22:29:43 -0400

    Project_0.s
    Added for loop to iterate through the address and print out the full string.

--- Project_0.s
+++ Project_0.s
@@ -6,9 +6,21 @@
 
 main:
 	la $t0, id		#loads the address of the id
+	addi $t1, $t0, 9	#sets $t1 to 9 plus $t0 so loop can reach $t1
+	j loop			#jumps to loop
+
+exit:
+	li $v0, 10		#ends the program
+	syscall
+	
+
+loop:
 	lb $a0, ($t0)		#loads a byte from the address
 	li $v0, 11		#prints the byte
 	syscall
+	addi $t0, $t0, 1	#adds 1 to $t0
 	
-	li $v0, 10		#ends the program
-	syscall
+	beq $t0, $t1, exit	#checks if $t0 equals to nine and exits the program
+	
+	j loop			#jumps to loop
+
