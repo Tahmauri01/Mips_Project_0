dda02c08fe408b43c94393e6ce7006cc41668b08
Tahmauri <tmbo718@gmail.com>
2025-03-12 17:37:59 -0400

    Added the print command, added operations for base address to get the next address and added subtraction operator to end the loop eventually

--- Project_0.s
+++ Project_0.s
@@ -31,3 +31,9 @@ print_substring:
 
 print_loop:
 	lb $a0, 0($t3)				#loading the character at this current address
+	li $v0, 11					#print command
+	syscall
+
+	addi $t3, $t3, 1			#adds 1 to the base address to get next address
+	addi $t4, $t4, -1			#subtracts one from counter to end the loop
+	beqz $t4, done				#checks if the counter is 0
